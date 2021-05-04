6fcc203f4ca8e494104a5686de9e075fb5adcd5b
IlanaVek <39160395+IlanaVek@users.noreply.github.com>
2021-05-04 15:52:08 +0300

    Update generic-update-script.rb

--- generic-update-script.rb
+++ generic-update-script.rb
@@ -140,7 +140,7 @@ commit = fetcher.commit
 # Parse the dependency files #
 ##############################
 puts "Parsing dependencies information"
-puts " user is ENV["GITHUB_ENTERPRISE_ACCESS_USER"] "
+puts " user is #{ENV['GITHUB_ENTERPRISE_ACCESS_USER']} "
 puts caller
 parser = Dependabot::FileParsers.for_package_manager(package_manager).new(
   dependency_files: files,
